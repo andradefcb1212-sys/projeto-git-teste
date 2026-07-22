66307a3338ef201d9331c7d4ab8dd3a7e2bc6b9a
andrade <andradefcb1212@gmail.com>
2026-07-22 15:40:39 -0400

    primeiro commit

---
+++ projeto01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main(){
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+
+    printf("Olá");
+    printf("Estou fazendo uma modificação no meu código.");
+}
